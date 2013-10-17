d026f8c2dbb3d20749e3751921db43a9603871bb
George Brocklehurst <george@georgebrock.com>
2013-10-17 16:21:39 +0200

    More talk setup gubbins.

---
+++ talk/bin/env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PS1='$ '
+
+export RUBYLIB=./lib
+export PATH=./bin:$PATH
+
+alias figlet='figlet -f roman'
---
+++ talk/template/lib/greeter/cli.rb
@@ -0,0 +1,9 @@
+require 'greeter'
+
+class Greeter
+  class CLI
+    def initialize(arguments)
+      @arguments = arguments
+    end
+  end
+end
